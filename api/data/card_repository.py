ecf05e76fa82f12fc2e2cd910b252d929ee1c408
Marc Donald <marcsdeveloperemail@gmail.com>
2020-10-30 12:34:59 +0000

    Separate presentation code from card repository

--- api/data/card_repository.py
+++ api/data/card_repository.py
@@ -9,17 +9,13 @@ cards = db.mtgCards
 def search_by_name(card_name, page_num, page_size):
     search_regex = re.compile("^.*{}.*$".format(card_name), re.IGNORECASE)
     page_start = get_page_start(page_size, page_num)
-    results_count = cards \
-        .count({"name": search_regex})
     search_results = cards \
         .find({"name": search_regex}) \
         .skip(int(page_start)) \
         .limit(int(page_size))
-    returned_page = {}
-    data = []
-    for result in search_results:
-        result['_id'] = str(result['_id'])
-        data.append(result)
-    returned_page['data'] = data
-    returned_page['count'] = results_count
-    return returned_page
+    return search_results
+
+
+def search_by_name_count(card_name):
+    search_regex = re.compile("^.*{}.*$".format(card_name), re.IGNORECASE)
+    return cards.count({"name": search_regex})
--- api/presenters/card_presenter.py
+++ api/presenters/card_presenter.py
@@ -2,4 +2,16 @@ from data import card_repository
 
 
 def search_by_name(card_name, page_num, page_size):
-    return card_repository.search_by_name(card_name, page_num, page_size)
+    search_result = card_repository.search_by_name(card_name, page_num, page_size)
+    result_count = card_repository.search_by_name_count(card_name)
+    returned_page = {}
+    data = []
+    for result in search_result:
+        card_id = str(result['_id'])
+        del result['_id']
+        result['id'] = card_id
+        data.append(result)
+    returned_page['data'] = data
+    returned_page['count'] = result_count
+    return returned_page
+
